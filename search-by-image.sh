85aae02de0696d6871df0df86088f8e2f1f44142
kcubeterm <kkrishna7638@gmail.com>
2020-04-27 18:16:40 +0530

    changes file

--- search-by-image.sh
+++ search-by-image.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # usage 
 # search-by-image /path/of/image.jpg
- 
-url=`curl -F encoded_image=@$1 https://www.google.de/searchbyimage/upload | grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' | sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' `
+kk="$@"
+url=`curl -F encoded_image=@"$kk" https://www.google.de/searchbyimage/upload | grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' | sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' `
 if [ -e $PREFIX/bin/xdg-open ] 
 then
 	xdg-open $url
